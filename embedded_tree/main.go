d79347ec16420e070c49670411ec0240b76f2f31
Ondrej Sika <ondrej@ondrejsika.com>
2022-08-22 21:35:59 +0200

    feat(embedded_tree): Add example with embedded file tree

---
+++ embedded_tree/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"embed"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+)
+
+//go:embed data/*
+//go:embed data/**/*
+var srcDataFS embed.FS
+
+func main() {
+	var err error
+	secFS, err := fs.Sub(srcDataFS, "data")
+	handleError(err)
+	fs.WalkDir(secFS, ".", func(filePath string, d fs.DirEntry, err error) error {
+		handleError(err)
+		if d.Type().IsDir() {
+			os.MkdirAll(path.Join("out", filePath), 0755)
+		} else {
+			file, err := secFS.Open(filePath)
+			handleError(err)
+			fileContent, err := ioutil.ReadAll(file)
+			handleError(err)
+			err = ioutil.WriteFile(path.Join("out", filePath), fileContent, 0644)
+			handleError(err)
+		}
+		return nil
+	})
+}
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
