cce55f74d51be45219fd73c0a197e9f564534f99
Ondrej Sika <ondrej@ondrejsika.com>
2023-04-29 10:16:30 +0200

    feat(function_alias): Add function_alias example

---
+++ function_alias/go.mod
@@ -0,0 +1,5 @@
+module function_alias
+
+go 1.19
+
+require github.com/ondrejsika/go-hello v0.1.0 // indirect
---
+++ function_alias/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/ondrejsika/go-hello"
+
+var h = hello.Hello
+
+func main() {
+	h()
+}
