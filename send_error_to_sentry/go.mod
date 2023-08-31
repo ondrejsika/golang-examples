5fbd81d815cf556928c0d975c6ea5ab48cf1035e
Ondrej Sika <ondrej@ondrejsika.com>
2023-08-31 23:03:20 +0200

    feat(send_error_to_sentry): Create example of Sentry client

---
+++ send_error_to_sentry/go.mod
@@ -0,0 +1,10 @@
+module hello_world
+
+go 1.19
+
+require github.com/getsentry/sentry-go v0.23.0
+
+require (
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
---
+++ send_error_to_sentry/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+)
+
+func main() {
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:   os.Getenv("SENTRY_DSN"),
+		Debug: true,
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	defer sentry.Flush(2 * time.Second)
+
+	sentry.CaptureMessage("It works!")
+}
