42ec2e5cda280d74f4ae189cf3028f044244923a
Ondrej Sika <ondrej@ondrejsika.com>
2025-04-25 09:52:40 +0200

    feat(go_gopher_example): Test my library ondrejsika/go-gopher for Gophercamp.cz

---
+++ go_gopher_example/go.mod
@@ -0,0 +1,5 @@
+module go_gopher_example
+
+go 1.24.1
+
+require github.com/ondrejsika/go-gopher v0.1.0
---
+++ go_gopher_example/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gopher "github.com/ondrejsika/go-gopher"
+)
+
+func main() {
+	fmt.Print(gopher.GOPHER)
+}
